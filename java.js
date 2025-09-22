da6466a3bf806c0d483a8b0aaf8c9421e9cbf602
realisation <souhailalh77@gmail.com>
2025-09-22 10:10:57 +0100

    ajout

--- java.js
+++ java.js
@@ -1,18 +1,33 @@
-let c12 = ["c1", "c3", "c2", "c1", "c8", "c1", "c2", "c5" ] ;
-let c={} ;
-for(let i=0;i<c12.length;i++){
-    let a=c12[i] ;
-    if(c[a]){
-        c[a]++ ;
+let valeurs=["c1","c3","c2","c1","c8","c1","c2","c5"];
+let codes={}
+for(let i=0;i<valeurs.length;i++){
+let list=valeurs[i];
+if(codes[list]){
+codes[list]++;
 
 
-    }else{
 
-        c[a]=1 ;
+}else{
 
-    }
+    codes[list]=1;
+
+
+
 }
-       
 
 
-console.log(c);
+
+
+}
+
+
+console.log(codes);
+
+
+
+
+
+
+
+
+
